30030a870802d76abf50af53d328c9f74e41b8d1
Siddhi yadav <113456549+Siddhi1kcr@users.noreply.github.com>
2025-08-07 15:21:05 +0530

    message

---
+++ frontend/my-app/src/components/molecules/Card.jsx
@@ -0,0 +1,12 @@
+import react from "react";
+import "../../styles/Card.css";
+
+function Card(){
+    return(
+        <div>
+            
+        </div>
+    );
+}
+
+export default Card;
---
+++ frontend/my-app/src/components/organs/ResultPage.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "../../styles/ResultPage.css";
+import Heading from "../atoms/Heading";
+import Graph from "../molecules/Graph";
+import Card from "../molecules/Card";
+
+function ResultPage() {
+  return (
+    <div className="Result-full-page">
+        <Heading />
+        <div className="content">
+           <div>
+           <div><Graph/></div>
+           <div><Graph/></div>
+           </div>
+           <div>
+           <div><Card/></div>
+           <div><Card/></div>
+           </div>
+        </div>
+    </div>
+  );
+}
