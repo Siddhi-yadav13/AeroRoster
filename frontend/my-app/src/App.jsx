9805900968de8312f1874421b9b09a5ebb7aaee4
sneha gupta <gsneha20012004@gmail.com>
2025-08-07 17:05:24 +0530

    whole page

--- frontend/my-app/src/App.jsx
+++ frontend/my-app/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import MainPage from "./components/organs/MainPage";
+import StationPage from './components/organs/StationPage';
 
 function App() {
   return (
     <>
       <MainPage/>
+      <StationPage/>
     </>
   )
 }
