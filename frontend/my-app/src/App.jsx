ab11b9b05caa2924c86bda060082b7757782b38e
Siddhi-yadav13 <SIDDHI.YADAV@goIndiGo.in>
2025-08-08 09:10:19 +0530

    jhjhjjj

--- frontend/my-app/src/App.jsx
+++ frontend/my-app/src/App.jsx
@@ -1,20 +1,13 @@
 import { useState } from 'react'
 import MainPage from "./components/organs/MainPage";
-<<<<<<< HEAD
 import ResultPage from "./components/organs/ResultPage";
-=======
 import StationPage from './components/organs/StationPage';
->>>>>>> 9805900968de8312f1874421b9b09a5ebb7aaee4
 
 function App() {
   return (
     <>
       <MainPage/>
-<<<<<<< HEAD
       <ResultPage/>
-=======
-      <StationPage/>
->>>>>>> 9805900968de8312f1874421b9b09a5ebb7aaee4
     </>
   )
 }
